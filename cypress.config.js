6d5a2f3f6f9bd226ec0cbdfbbf81529e4d7776b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-31 17:58:55 -0400

    added plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,24 @@
 const { defineConfig } = require('cypress')
 
+// https://github.com/bahmutov/cypress-visited-urls
+const visitedUrlsPlugin = require('cypress-visited-urls/src/plugin')
+
 module.exports = defineConfig({
   defaultBrowser: 'electron',
-  e2e: {},
+  e2e: {
+    env: {
+      visitedUrls: {
+        // collect each URL the test runner visits
+        // https://glebbahmutov.com/blog/collect-tested-urls/
+        collect: true,
+        urlsFilename: 'cypress-visited-urls.json',
+      },
+    },
+    setupNodeEvents(cypressOn, config) {
+      visitedUrlsPlugin(on, config)
+      // IMPORTANT to return the config object
+      // with the any changed environment variables
+      return config
+    },
+  },
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,7 @@
 // https://github.com/bahmutov/cypress-rest-easy
 import 'cypress-rest-easy'
+
+// https://glebbahmutov.com/blog/collect-tested-urls/
+import { configureVisitedUrls } from 'cypress-visited-urls'
+
+configureVisitedUrls()
